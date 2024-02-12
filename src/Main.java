6206ea4e1b5fda73495e7c7224a0610449e7aac9
zenisekm <zenisek96@gmail.com>
2024-02-12 20:43:55 +0100

    Lekce 1, Domácí úkol část 2. Mrkve.
    Zkouška uploadu na Git, uložení.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import com.engeto.ja.Employee;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee employee1 =
+                new Employee("Franta Vopršálek",
+                        LocalDate.of(1987,1,13),
+                        9,90,
+                        "Brno", "9H97903", 5,
+                        844.785.444.777);
+
+
+
+
+
+
+        Employee employee2 = new Employee();
+        Employee employee3 = new Employee();
+
+        employee1.setName("Franta Vopršálek");
+        System.out.println(employee1.getName());
+
+
+
+
     }
 }
+
--- src/com/engeto/ja/Employee.java
+++ src/com/engeto/ja/Employee.java
@@ -1,2 +1,105 @@
-package com.engeto.ja;public class Employee {
+package com.engeto.ja;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Employee {
+
+    private String name;
+    private LocalDate birthdate;
+    private int contracts;
+    private BigDecimal carrotsWeight;
+    private String city;
+    private String spzNumber;
+    private BigDecimal consumption;
+    private int ipAddress;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public int getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(int contracts) {
+        this.contracts = contracts;
+    }
+
+    public BigDecimal getCarrotsWeight() {
+        return carrotsWeight;
+    }
+
+    public void setCarrotsWeight(BigDecimal carrotsWeight) {
+        this.carrotsWeight = carrotsWeight;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSpzNumber() {
+        return spzNumber;
+    }
+
+    public void setSpzNumber(String spzNumber) {
+        this.spzNumber = spzNumber;
+    }
+
+    public BigDecimal getConsumption() {
+        return consumption;
+    }
+
+    public void setConsumption(BigDecimal consumption) {
+        this.consumption = consumption;
+    }
+
+    public int getIpAddress() {
+        return ipAddress;
+    }
+
+    public Employee(String name, LocalDate birthdate,
+                    int contracts, BigDecimal carrotsWeight,
+                    String city, String spzNumber,
+                    BigDecimal consumption, int ipAddress) {
+        this.name = name;
+        this.birthdate = birthdate;
+        this.contracts = contracts;
+        this.carrotsWeight = carrotsWeight;
+        this.city = city;
+        this.spzNumber = spzNumber;
+        this.consumption = consumption;
+        this.ipAddress = ipAddress;
+    }
+
+    public void setIpAddress(int ipAddress) {
+        this.ipAddress = ipAddress;
+
+
+
+
+
+
+
+
+
+
+    }
 }
+
